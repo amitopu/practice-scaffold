c34df7c3c8a749ab57dc79fd98e730e28ffe939d
Md Nur E Alam Siddiky <nurealamsiddiky@gmail.com>
2024-03-09 04:04:15 +0000

    new commit with bug fix

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+import random
+import math
 def add(x, y):
     return x + y
 
 
-num1 = 12
-num2 = 14
+num1 = math.floor(random.random() * 1000)
+num2 = math.floor(random.random() * 1000)
 result = add(num1, num2)
-print(f"Sun of ${num1} and {num2} is {result}")
+print(f"Sun of {num1} and {num2} is {result}")
