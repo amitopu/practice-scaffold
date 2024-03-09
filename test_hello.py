46618722f6ad92a9aca5aa3d1bf79928208b1a8c
Md Nur E Alam Siddiky <nurealamsiddiky@gmail.com>
2024-03-09 04:17:46 +0000

    making test passed

--- test_hello.py
+++ test_hello.py
@@ -2,7 +2,7 @@ import hello
 
 
 def test_add():
-    assert hello.add(1234, 6758) == 1234 + 67586
+    assert hello.add(1234, 67588) == 1234 + 67588
     assert hello.add(-869, 7656) == (-869) + 7656
     assert hello.add(-98, -756) == (-98) + (-756)
     assert hello.add(0, 65) == 0 + 65
